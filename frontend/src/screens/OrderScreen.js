e8e91fb809885f985423c9e1c22ea54ad0b6eb66
AmirHossein Esmailie <69587936+amirhosseine13579@users.noreply.github.com>
2021-09-16 21:12:25 +0430

    commit

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 import { deliverOrder, detailsOrder, payOrder } from '../actions/orderActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
-import ZarinPalCheckout from 'zarinpal-checkout';
+// import ZarinPalCheckout from 'zarinpal-checkout';
 
 import {
   ORDER_DELIVER_RESET,
@@ -22,30 +22,30 @@ export default function OrderScreen(props) {
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   
-/**
- * Create ZarinPal
- * @param {String} `1aaccd0b-9c1b-405c-8952-f21f8bd277bc` [Merchant ID]
- * @param {Boolean} false [toggle `Sandbox` mode]
- */
- const zarinpal = ZarinPalCheckout.create('1aaccd0b-9c1b-405c-8952-f21f8bd277bc', false);
+// /**
+//  * Create ZarinPal
+//  * @param {String} `1aaccd0b-9c1b-405c-8952-f21f8bd277bc` [Merchant ID]
+//  * @param {Boolean} false [toggle `Sandbox` mode]
+//  */
+//  const zarinpal = ZarinPalCheckout.create('1aaccd0b-9c1b-405c-8952-f21f8bd277bc', false);
   
- /**
- * PaymentRequest [module]
- * @return {String} URL [Payement Authority]
- */
-zarinpal.PaymentRequest({
-  Amount: '1000', // In Tomans
-  CallbackURL: 'https://api.zarinpal.com/pg/v4/payment/request.json',
-  Description: 'A Payment from Node.JS',
-  Email: 'hi@siamak.work',
-  Mobile: '09120000000'
-}).then(response => {
-  if (response.status === 200) {
-    console.log(response.url);
-  }
-}).catch(err => {
-  console.error(err);
-});
+//  /**
+//  * PaymentRequest [module]
+//  * @return {String} URL [Payement Authority]
+//  */
+// zarinpal.PaymentRequest({
+//   Amount: '1000', // In Tomans
+//   CallbackURL: 'https://api.zarinpal.com/pg/v4/payment/request.json',
+//   Description: 'A Payment from Node.JS',
+//   Email: 'hi@siamak.work',
+//   Mobile: '09120000000'
+// }).then(response => {
+//   if (response.status === 200) {
+//     console.log(response.url);
+//   }
+// }).catch(err => {
+//   console.error(err);
+// });
 
  //  const script = document.createElement('script');
   // script.type = 'text/javascript';
@@ -246,7 +246,7 @@ zarinpal.PaymentRequest({
                         <button
                           
                           type="submit"
-                          onClick={zarinpal.PaymentRequest}
+                          // onClick={zarinpal.PaymentRequest}
                           className="primary block peyment"
                         >
                           پرداخت
